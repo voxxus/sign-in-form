49d42eeea12f48dd475a68702fa39e3173418c9b
Aleksandr Sukhov <sukhov.aa@bk.ru>
2023-04-11 22:13:33 +0300

    decompose scripts and styles

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { startApp } from './scripts/app.js';
+import { enableThemes } from './scripts/themes.js';
+
+startApp('#app');
+
+enableThemes();
---
+++ src/scripts/app.js
@@ -0,0 +1,7 @@
+import { renderRegisterForm } from './form.js';
+
+export function startApp(cssSelector) {
+    const app = document.querySelector(cssSelector);
+
+    app.appendChild(renderRegisterForm());
+}
