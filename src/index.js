238149e3b88db9ca11ac478ffdc1530f3a402bd6
Aleksandr Sukhov <sukhov.aa@bk.ru>
2023-04-12 19:52:06 +0300

    add webpack config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { startApp } from './scripts/app.js';
-import { enableThemes } from './scripts/themes.js';
+import { startApp } from '@/scripts/app.js';
+import { enableThemes } from '@/scripts/themes.js';
+
+import '@/assets/styles/index.css';
 
 startApp('#app');
 
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        filename: 'js/bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'media/[hash][ext][query]',
+        clean: true,
+    },
+
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src/'),
+        },
+    },
+
+    devtool: 'source-map',
+
+    devServer: {
+        static: './public',
+        hot: true,
+        watchFiles: ['./src/index.html'],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({ title: 'Register & Login UI Kit', template: './src/index.html' }),
+        new MiniCssExtractPlugin({ filename: 'css/main.css' }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env'],
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: ['postcss-preset-env'],
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
