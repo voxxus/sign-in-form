83035f1fda6a1ea6e33eb2afe3ee66d38aab332c
Aleksandr Sukhov <sukhov.aa@bk.ru>
2023-04-15 22:47:14 +0300

    fix indents

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -3,59 +3,64 @@ export function renderRegisterForm() {
   form.classList.add('form', 'form--register');
 
   form.innerHTML =
-        `<h2 class="form__title">Register</h2>
+    `<h2 class="form__title">Register</h2>
 
-        <div class="form__item-block">
-            <label class="form__item">
-                <input
-                    type="text"
-                    class="form__item-input form__item-input--register"
-                    name="fullName"
-                    placeholder="Full Name"
-                >
-            </label>
+    <div class="form__item-block">
+      <label class="form__item">
+        <input
+          type="text"
+          class="form__item-input form__item-input--register"
+          name="fullName"
+          placeholder="Full Name"
+        >
+        <div class="form__item-error">ss</div>
+      </label>
 
-            <label class="form__item">
-                <input
-                    type="email"
-                    class="form__item-input form__item-input--register"
-                    name="email"
-                    placeholder="Email"
-                >
-            </label>
+      <label class="form__item">
+        <input
+          type="email"
+          class="form__item-input form__item-input--register"
+          name="email"
+          placeholder="Email"
+        >
+        <div class="form__item-error">ss</div>
+      </label>
 
-            <label class="form__item">
-                <input
-                    type="tel"
-                    class="form__item-input form__item-input--register"
-                    name="phoneNumber"
-                    placeholder="Phone"
-                >
-            </label>
+      <label class="form__item">
+        <input
+          type="tel"
+          class="form__item-input form__item-input--register"
+          name="phoneNumber"
+          placeholder="Phone"
+        >
+        <div class="form__item-error">ss</div>
+      </label>
 
-            <label class="form__item">
-                <input
-                    type="password"
-                    class="form__item-input form__item-input--register"
-                    name="password"
-                    placeholder="Password"
-                >
-            </label>
+      <label class="form__item">
+        <input
+          type="password"
+          class="form__item-input form__item-input--register"
+          name="password"
+          placeholder="Password"
+        >
+        <div class="form__item-error">ss</div>
+      </label>
 
-            <label class="form__item">
-                <input
-                    type="password"
-                    class="form__item-input form__item-input--register"
-                    name="passwordConfirm"
-                    placeholder="Confirm Password"
-                >
-            </label>
-        </div>
+      <label class="form__item">
+        <input
+          type="password"
+          class="form__item-input form__item-input--register"
+          name="passwordConfirm"
+          placeholder="Confirm Password"
+        >
+        <div class="form__item-error">ss</div>
+      </label>
+    </div>
 
-        <div class="form__button-block">
-            <button type="submit" class="form__button form__button--register">Register</button>
-            <button type="button" class="form__button form__button--sign-in">Have account? Sign in</button>
-        </div>`;
+    <div class="form__button-block">
+      <button type="submit" class="form__button form__button--register">Register</button>
+      <button type="button" class="form__button form__button--sign-in">Have account? Sign in</button>
+    </div>`;
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -78,31 +83,33 @@ function renderLoginForm() {
   form.classList.add('form', 'form--login');
 
   form.innerHTML =
-        `<h2 class="form__title">Login</h2>
+    `<h2 class="form__title">Login</h2>
 
-        <div class="form__item-block">
-            <label class="form__item">
-                <input
-                    type="text"
-                    class="form__item-input form__item-input--login"
-                    name="username"
-                    placeholder="Username"
-                >
-            </label>
+    <div class="form__item-block">
+      <label class="form__item">
+        <input
+          type="text"
+          class="form__item-input form__item-input--login"
+          name="username"
+          placeholder="Username"
+        >
+        <div class="form__item-error">ss</div>
+      </label>
 
-            <label class="form__item">
-                <input
-                    type="password"
-                    class="form__item-input form__item-input--login"
-                    name="password"
-                    placeholder="Password"
-                >
-            </label>
-        </div>
+      <label class="form__item">
+        <input
+          type="password"
+          class="form__item-input form__item-input--login"
+          name="password"
+          placeholder="Password"
+        >
+        <div class="form__item-error">ss</div>
+      </label>
+    </div>
 
-        <div class="form__button-block form__button-block--login">
-            <button type="submit" class="form__button form__button--login">Login</button>
-        </div>`;
+    <div class="form__button-block form__button-block--login">
+      <button type="submit" class="form__button form__button--login">Login</button>
+    </div>`;
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
